6b23836a4afa8a8bf260e2fa0e3a56b6e6388de9
James Garner <jim.garner@live.co.uk>
2025-07-06 20:56:22 +0100

    Lesson 8 - Inputs commented

--- Day 1/day_one.py
+++ Day 1/day_one.py
@@ -1,5 +1,11 @@
+## Print can be augmented with \n for line breaks - as with JS
+
 print("Hello World!\nHello World!\nHello World!")
 
+## Concatenation is the same as JavaScript really.
+
 print("Hello " + "James")
+
+## Printing can also directly print the result of an input as below (Use vars instead though, obvs).
 
 print(input("What's your name?"))
