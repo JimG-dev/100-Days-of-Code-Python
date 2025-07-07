32280e5e075dfd5512eed21d26b4e855c00dcc19
James Garner <jim.garner@live.co.uk>
2025-07-07 20:56:20 +0100

    Lesson 15: Data types and Type Casting

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,7 +1,28 @@
-### Dissecting strings - below grabs the first char
+# ### Dissecting strings - below grabs the first char
+#
+# print("Hello"[0])
+#
+# ### Below grabs the last chat - this is semi-dynamic
+#
+# print("Hello"[-1])
+#
 
-print("Hello"[0])
+### Data Types
 
-### Below grabs the last chat - this is semi-dynamic
+print(type(123)) #int
 
-print("Hello"[-1])
+print(type("123")) #string
+
+print(type(True)) #boolean
+
+print(type(3.14)) #float
+
+### Type Casting
+
+print(int("123")) #casts to int
+
+### Challenge
+
+length_of_name = len(input("Enter your name:\n"))
+
+print(f"Number of letters in your name: {length_of_name}")
