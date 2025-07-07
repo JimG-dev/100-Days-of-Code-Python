5bdf0bc77594d26eaa715e4e0ce2bb24d52662a7
James Garner <jim.garner@live.co.uk>
2025-07-07 21:07:07 +0100

    Lesson 17: Rounding

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -7,22 +7,28 @@
 # print("Hello"[-1])
 #
 
-### Data Types
+# ### Data Types
+#
+# print(type(123)) #int
+#
+# print(type("123")) #string
+#
+# print(type(True)) #boolean
+#
+# print(type(3.14)) #float
+#
+# ### Type Casting
+#
+# print(int("123")) #casts to int
+#
+# ### Challenge
+#
+# length_of_name = len(input("Enter your name:\n"))
+#
+# print(f"Number of letters in your name: {length_of_name}")
 
-print(type(123)) #int
-
-print(type("123")) #string
-
-print(type(True)) #boolean
-
-print(type(3.14)) #float
-
-### Type Casting
-
-print(int("123")) #casts to int
-
-### Challenge
-
-length_of_name = len(input("Enter your name:\n"))
-
-print(f"Number of letters in your name: {length_of_name}")
+# ### Rounding to two decimal places | round(value, digits) |
+#
+# bmi = 84 / (1.65  ** 2)
+#
+# print(round(bmi, 2))
