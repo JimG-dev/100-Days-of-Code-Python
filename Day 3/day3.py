9cd51f6b8f616707df8f23a80672afb6a598996c
James Garner <jim.garner@live.co.uk>
2025-07-10 23:01:36 +0100

    Lesson 22: If Else intro

---
+++ Day 3/day3.py
@@ -0,0 +1,10 @@
+### If/else
+
+print("Welcome to the Rollercoaster!")
+
+height = int(input("What's your height? \n"))
+
+if height >= 120:
+    print("You can ride!!")
+else:
+    print("No ride for you!!")
